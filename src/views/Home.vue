5be59a875ef2a3fd54868dda6de13050de140179
Jordan <jcharrison12@gmail.com>
2021-07-30 16:40:16 -0400

    Adds current user's gallery to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
     <h1>Your Gallery</h1>
-    <div v-for="image in galleries.images" :key="image.id">
-      <img :src="image.image_url" />
+    <!-- <h2>{{ user }}</h2> -->
+    <!-- <div v-for="image in user.images" :key="image.id"> -->
+    <div v-for="(gallery, index) in user.galleries" :key="gallery.id">
+      <img :src="images[index].image_url" />
+      <h2>{{ gallery.notes }}</h2>
     </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <style></style>
@@ -15,7 +19,8 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      galleries: [],
+      user: [],
+      images: [],
     };
   },
   created: function () {
@@ -29,7 +34,8 @@ export default {
         .get("/users/" + user_id)
         .then((response) => {
           console.log(response.data);
-          this.galleries = response.data;
+          this.user = response.data;
+          this.images = response.data.images;
         })
         .catch((error) => {
           console.log(error);
---
+++ src/views/ImagesShow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="images-show">
+    <div class="container">
+      <h2>{{ image.title }}</h2>
+      <img :src="image.image_url" :alt="image.title" />
+      <h2>{{ image.artist }}</h2>
+      <p>{{ image.museum }}</p>
+      <p>{{ image.description }}</p>
+      <router-link to="/images">Back to all images</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      image: [],
+    };
+  },
+  created: function () {
+    axios.get("/images/" + this.$route.params.id).then((response) => {
+      this.image = response.data;
+      console.log(response.data);
+    });
+  },
+  methods: {},
+};
+</script>
