99be51033f2856e1b9f4ee4934ecdcf5acd61015
Jordan <jcharrison12@gmail.com>
2021-08-09 10:55:08 -0500

    Adds back unordered home page functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
     <h1>Your Gallery</h1>
-    <!-- <h2>{{ user }}</h2> -->
-    <!-- <div v-for="image in user.images" :key="image.id"> -->
+    <h2>{{ user }}</h2>
+    <!-- <div v-for="image in user.images" :key="image.id">
+      <div v-for="index in indexes" :key="index.id">
+        <h2>{{ index.image }}</h2>
+        <h2>{{ index.notes }}</h2>
+      </div> -->
     <div v-for="(gallery, index) in user.galleries" :key="gallery.id">
+      <!-- <div>{{ user }}</div> -->
       <img :src="images[index].image_url" />
       <h1>{{ images[index].title }}</h1>
       <h2>{{ images[index].artist }}</h2>
@@ -20,7 +25,6 @@
       </p>
     </div>
   </div>
-  <!-- </div> -->
 </template>
 
 <style></style>
@@ -33,11 +37,23 @@ export default {
     return {
       user: [],
       images: [],
+      indices: [],
+      array: [],
     };
   },
   created: function () {
     this.userHome();
   },
+  // computed: {
+  //   indexes() {
+  //     return this.indices.map((index, i) => {
+  //       return {
+  //         image: this.images[index],
+  //         notes: this.galleries[i].notes,
+  //       };
+  //     });
+  //   },
+  // },
   methods: {
     userHome: function () {
       var user_id = this.$parent.getUserId();
@@ -48,11 +64,16 @@ export default {
           console.log(response.data);
           this.user = response.data;
           this.images = response.data.images;
+          this.indices = response.data.galleries.map((object) => object.image_id);
+          console.log(this.indices);
         })
         .catch((error) => {
           console.log(error);
         });
     },
+    // newArray: function () {
+    // //   // this.array = this.indices.map((index) => {});
+    // },
   },
 };
 </script>
