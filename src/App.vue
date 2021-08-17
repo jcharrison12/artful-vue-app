a9ac37f6db0cb06d126623d9451dcc0e584260eb
Jordan <jcharrison12@gmail.com>
2021-08-17 11:23:08 -0500

    starts to incorporate bootstrap theme, adds navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,74 @@
 <template>
   <div id="app">
+    <header id="header">
+      <div class="container header-top">
+        <div class="row">
+          <div class="col-6 top-head-left">
+            <ul>
+              <li><a href="#">Visit Us</a></li>
+              <li><a href="#">Buy Ticket</a></li>
+            </ul>
+          </div>
+          <div class="col-6 top-head-right">
+            <ul>
+              <li>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-behance"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="container">
+        <div class="row align-items-center justify-content-between d-flex">
+          <!-- <div id="logo">
+            <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+          </div> -->
+          <nav id="nav-menu-container">
+            <ul class="nav-menu">
+              <li class="menu-active"><a href="/">Home</a></li>
+              <li><a href="/galleries">Galleries</a></li>
+              <li><a href="/images">Images</a></li>
+              <li v-if="!isLoggedIn()"><a href="/signup">Signup</a></li>
+              <li v-if="!isLoggedIn()"><a href="/login">Login</a></li>
+              <li v-if="isLoggedIn()"><a href="/logout">Logout</a></li>
+              <li class="menu-has-children">
+                <a href="">Pages</a>
+                <ul>
+                  <li><a href="blog-single.html">Blog Single</a></li>
+                  <li><a href="category.html">Category</a></li>
+                  <li><a href="elements.html">Elements</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <!-- #nav-menu-container -->
+        </div>
+      </div>
+    </header>
+    <!-- start banner Area -->
+    <section class="banner-area relative" id="home">
+      <div class="overlay overlay-bg"></div>
+      <div class="container">
+        <div class="row fullscreen d-flex align-items-center justify-content-center">
+          <div class="banner-content col-lg-8">
+            <h1 class="text-white">Artful: An Art Appreciation App</h1>
+            <p class="pt-20 pb-20 text-white">Save your favorite pieces of art in your very own personal gallery!</p>
+            <a href="#" class="primary-btn text-uppercase">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End banner Area -->
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
@@ -19,28 +88,7 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 <script>
 export default {
   methods: {
